ff5f4fd114196ebce69355d6f2164189241368e3
TVBlackman1 <41297903+TVBlackman1@users.noreply.github.com>
2019-01-05 21:53:38 +0300

    Update doImage.py

--- doImage.py
+++ doImage.py
@@ -1,86 +1,37 @@
 from PIL import Image
 
+IMAGES_PATH = 'images/'
 
+file_open = "alex.jpg"  # input("Name of file: ")
+file_load = "file.txt"  # input("Name of text file: ")
 
-
-
-fileOpen = input("Name of file: ")
-fileLoad = input("Name of text file: ")
-orientation = input("What`s orientation(normal|90○ left)? Enter 1 or 2 ")
-
-
-
-
-
-image = Image.open('photos/'+fileOpen)
-width = image.size[0]  
-height = image.size[1] 	
-pix = image.load() #Выгружаем значения пикселей.
+image = Image.open(IMAGES_PATH + file_open)
+width_image = image.size[0]
+height_image = image.size[1]
+pixels_class = image.load()  # unload pixels
 
 variants = ["♦", "#", "$", "&", "N", "%", "M", "H", "P", "K", \
-        "Z", "T", "?", "/", ":", "."]
-#variants = ["◘","♦", "#", "$", "&", "N", "%", "M", "H", "P", "K", \
- #       "Z", "T", "?", "/", ":", ".", " "]   # Try more elements
+            "Z", "T", "?", "/", ":", "."]
+
+# variants = "QWERTYUIOP[]';LKJHGFDSAZXCBNM,./"  # for the thrill
+coefficient = len(variants)
 
 
-files = {}
+def txt_file_by_image(pixels, size_x: int, size_y: int):
+    global height_image, width_image
 
-def doByI(i):
-  print('function started')
-  global files
-  for j in range(height):
-      a = pix[i, j][0]
-      b = pix[i, j][1]
-      c = pix[i, j][2]
-      S = (a + b + c) // 3
-      number = int(S // len(variants))
-      files[(i, j)] = variants[number]
-  if i == width - 1:
-    return files
-      
-def main():
-  global files
-  List = list(range(width))
-  print('map activated')
-  for i in List:
-    doByI(i)
-  print('map end')
-  a = files
-  print(a)
-  print(len(a))
-  input("Press the button, please")
-  with open(fileLoad, "w") as file:
-    pass
-  ForFile = ""
-  allOf = width * height
-  if orientation == "1":
-    for j in range(height):
-      ForFile += "\n"
-      for i in range(width):
-        print(j * width + i, '/', allOf, str(int((j * width + i) / allOf * 100)) + "%")
-        ForFile += a[(i, j)]
-        print(a[(i, j)])
-      if len(ForFile) >= 1000:
-          with open(fileLoad, "a", encoding = "utf-8") as file:  
-              file.write(ForFile)
-              ForFile = ""
-  elif orientation == "2":
-    for i in range(width):
-      ForFile += "\n"
-      for j in range(height):
-        print(i * height + j, '/', allOf, str(int((i * height + j) / allOf * 100)) + "%")
-        ForFile += a[(i, j)]
-        print(a[(i, j)])
-      if len(ForFile) >= 1000:
-          with open(fileLoad, "a", encoding = "utf-8") as file:  
-              file.write(ForFile)
-              ForFile = ""
-  else:
-    print("Your orientation is not defined.")
-  print('End')
+    returned: str = ""
 
-    
+    for y in range(size_y):
+        for x in range(size_x):
+            r, g, b = pixels[x, y]
+            index = ((r + g + b)//3 * coefficient) // 255 - 1
+            # print(index)
+            returned += variants[index]
+        returned += "\n"
+    with open(file_load, "w", encoding="utf-8") as file_output:
+        file_output.write(returned)  # Clean the document and print result
 
-if __name__ == "__main__":
-  main()
+
+txt_file_by_image(pixels=pixels_class, size_x=width_image, size_y=height_image)
 
